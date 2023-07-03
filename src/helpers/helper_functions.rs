e5b1c9308655dc2fa7283c221c4adf1e45831d9a
Ramone Henry <ramoneh94@gmail.com>
2023-07-03 18:27:44 -0400

    start add helper, file cleanup

---
+++ src/helpers/helper_functions.rs
@@ -0,0 +1,70 @@
+use reqwest::{Client, header::{}};
+use search_response::{TeamSearchResponse, TeamInfo};
+use fixture_response::{FixtureResponse, FixtureData};
+use std::{};
+use chrono::{TimeZone, Utc};
+
+use crate::{search_response, fixture_response};
+
+
+
+
+static mut TEAM_ID: u32 = 0;
+
+
+pub fn convert_timestamp(timestamp: u64) -> String {
+        let dt = Utc.timestamp(timestamp.try_into().unwrap(), 0);
+        dt.format("%-I:%M %p").to_string()
+    }
+
+
+pub fn print_sports_info(teams: Vec<TeamInfo>, country: &str) {// Borrow the value using a reference
+    for team in teams {
+        if team.team.country == country {
+            {
+                
+                println!("team name: {}", team.team.name);
+                println!("team id: {}", team.team.id);
+                println!("team country: {}", team.team.country);
+                println!("---------");
+            }
+            unsafe { TEAM_ID = team.team.id };
+            return; // only get the first team information 
+           
+        } 
+    }
+    
+}
+
+pub fn print_fixtures_info(teams: Vec<FixtureData>, team_id: u32) {// Borrow the value using a reference
+    for team in teams {
+        if team.teams.home.id == team_id  || team.teams.home.id == team_id {
+            {
+                
+                println!("Match time: {}", convert_timestamp(team.fixture.timestamp));
+                println!("Match day: {}", team.fixture.date);
+                println!("Match location: {:#?}", team.fixture.venue.name);
+                println!("{}: {}", team.teams.home.name, team.goals.home);
+                println!("{}: {}", team.teams.away.name, team.goals.away);
+                println!("{}, Season: {}", team.league.name, team.league.season);
+                println!("Referee: {:?}", team.fixture.referee);
+                println!("---------");
+            }        
+        }
+    }
+    
+}
+
+pub fn get_team_name() -> String{
+   let mut line = String::new();
+   println!("Enter your team name: ");
+   std::io::stdin().read_line(&mut line).unwrap();
+   return line.trim().to_string();
+}
+
+pub fn get_team_country() -> String{
+   let mut line = String::new();
+   println!("Enter your team country: ");
+   std::io::stdin().read_line(&mut line).unwrap();
+   return line.trim().to_string();
+}
